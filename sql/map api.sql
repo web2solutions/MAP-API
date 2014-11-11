2c76e0a5b055c45a2c55f039107aacb7a1dc312a
Eduardo Almeida <eduardo@web2solutions.com.br>
2014-11-11 16:51:19 -0200

    Update map api.sql

--- sql/map api.sql
+++ sql/map api.sql
@@ -1,19 +1,6 @@
 ﻿use MAPTEST; 
 
 
-IF OBJECT_ID('dbo.tbl_api_secret', 'U') IS NOT NULL
-          DROP TABLE dbo.tbl_api_secret;
-CREATE TABLE dbo.tbl_api_secret( 
-        api_secret_id INT not null Identity(1,1),
-        user_id integer NOT NULL,
-        username  varchar(max), -- user email SHA256 format
-        first_name  varchar(max), -- user first and last name
-        CONSTRAINT tbl_api_secret_pkey PRIMARY KEY (api_secret_id)
-);
-
--- ALTER TABLE dbo.tbl_api_secret ADD CONSTRAINT FK_user_accounts_user_id_tbl_api_secret_user_id FOREIGN KEY(user_id) REFERENCES user_accounts(user_id);
-
-
 IF OBJECT_ID('dbo.tbl_api_access_token', 'U') IS NOT NULL
           DROP TABLE dbo.tbl_api_access_token;
 CREATE TABLE dbo.tbl_api_access_token( 
@@ -24,16 +11,6 @@ CREATE TABLE dbo.tbl_api_access_token(
         date_expiration bigint, -- I want epoch time here (seconds) -- default today + 1 day
         active_status integer NOT NULL default 0,
         CONSTRAINT tbl_api_access_token_pkey PRIMARY KEY (api_access_token_id)
-);
-
-IF OBJECT_ID('dbo.tbl_api_access_log', 'U') IS NOT NULL
-          DROP TABLE dbo.tbl_api_access_log;
-CREATE TABLE dbo.tbl_api_access_log( 
-        api_access_log_id INT not null Identity(1,1),
-        user_id integer NOT NULL,
-        token  varchar(max),
-        date_authentication datetime, -- I want epoch time here (seconds)
-        CONSTRAINT api_access_log_pkey PRIMARY KEY (api_access_log_id)
 );
 
 
@@ -58,14 +35,9 @@ insert into tbl_api_allowed_origin(origin) values('https://cdmap03.myadoptionpor
 --insert into tbl_api_allowed_origin(origin) values('http://aai.myadoptionportal.com');
 --insert into tbl_api_allowed_origin(origin) values('https://aai.myadoptionportal.com');
 
+----- END
 
 
-
-select * from user_accounts;
-
-INSERT tbl_api_secret (user_id, username, first_name)
-  SELECT user_id,  HASHBYTES('SHA2_256',username ), first_name
-  FROM user_accounts
 
 
 
