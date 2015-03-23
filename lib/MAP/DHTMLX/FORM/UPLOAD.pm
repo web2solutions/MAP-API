3e40f094bedc0d3e67e7b9eba2095a7d60ba8b91
web2solutions <eduardo@web2solutions.com.br>
2015-03-23 14:37:16 -0300

    upload files end point
    - defined logic for storing files
    - implemented authorization for upload files end point

--- lib/MAP/DHTMLX/FORM/UPLOAD.pm
+++ lib/MAP/DHTMLX/FORM/UPLOAD.pm
@@ -4,21 +4,36 @@ use Data::Dump qw(dump);
 our $VERSION = '0.1';
 
 prefix '/dhtmlx/form';
-my $root_dir = '/var/www/html/userhome/FormBuilder/';
+my $root_dir = '/var/www/html/userhome/upload_files/';
+my $web_path = 'userhome/upload_files/';
 
 options '/upload.:format' => sub {
 	MAP::API->options_header();
 };
 
-post '/upload.:format' => sub{ 
-    #MAP::API->check_authorization( params->{token}, request->header("Origin") );
+post '/upload.:format' => sub{
+    MAP::API->check_authorization_simple( params->{token}, request->header("Origin") );
     MAP::API->normal_header();
 
-    my $path = $root_dir;
-    my $filename = params->{file};
-    
+
+    my $agency_id = params->{agency_id};
+		my $user_id = params->{user_id};
+		my $path = $root_dir . '/' . $agency_id . '/';
+		my $filename = $agency_id  . '_' . $user_id . '_' . time . '_' . params->{file};
+
+
+		unless(-e $root_dir){
+			mkdir($root_dir, 0777);
+		}
+
+
+		unless(-e $path){
+			mkdir($path, 0777);
+		}
+
+
     if ( params->{mode} eq "html5" || params->{mode} eq "flash" ) {
-        my $uploaded_file = request->upload('file'); 
+        my $uploaded_file = request->upload('file');
         $uploaded_file->copy_to($path . $filename);
         #debug "My Log 2: " . ref($uploaded_file);
         return {
@@ -26,14 +41,14 @@ post '/upload.:format' => sub{
             name => $filename
         };
     }
-    
+
     if ( params->{mode} eq "html4" ) {
         if ( params->{actions} eq "cancel" ) {
             return {    state => 'cancelled'    };
         }
         else
         {
-            my $uploaded_file = request->upload('file'); 
+            my $uploaded_file = request->upload('file');
             $uploaded_file->copy_to($path . $filename);
             return {
                 state => true,
