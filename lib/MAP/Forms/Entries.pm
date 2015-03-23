e2ef0e2bbf5abf0c4ad136cf2ea3769bd98b13bb
web2solutions <eduardo@web2solutions.com.br>
2015-03-23 14:41:20 -0300

    Entries end point changes

--- lib/MAP/Forms/Entries.pm
+++ lib/MAP/Forms/Entries.pm
@@ -31,46 +31,31 @@ get '/'.$collectionName.'/:'.$primaryKey.'/:type.:format' => sub {
     my $formname = MAP::API->SelectOne("SELECT formname FROM $tableName
                                        WHERE form_id = ?",
                                        $item_id) || MAP::API->fail( "formname not exist" );
-    
+
     my $agency_id = request->header("X-AId") || MAP::API->fail( "please provide agency_id" );
-    
+
     my $strSQLtable = '
         IF OBJECT_ID(\'dbo.formmaker_'.$agency_id .'_'.$formname.'\', \'U\') IS NULL
-            CREATE TABLE dbo.formmaker_'.$agency_id .'_'.$formname.'( 
+            CREATE TABLE dbo.formmaker_'.$agency_id .'_'.$formname.'(
                     [data_id] INT not null Identity(1,1),
                     [user_id] INT,
                     [key_id] INT,
                     [connId] INT,
-                    [connectionId] INT, 
+                    [connectionId] INT,
                     CONSTRAINT formmaker_'.$agency_id .'_'.$formname.'_pkey PRIMARY KEY (data_id)
         );
-        IF OBJECT_ID(\'dbo.formmaker_'.$agency_id .'_'.$formname.'_tmp\', \'U\') IS NULL
-            CREATE TABLE dbo.formmaker_'.$agency_id .'_'.$formname.'_tmp( 
-                    [data_id] INT not null Identity(1,1),
-                    [user_id] INT,
-                    [key_id] INT,
-                    [connId] INT,
-                    [connectionId] INT, 
-                    CONSTRAINT formmaker_'.$agency_id .'_'.$formname.'_tmp_pkey PRIMARY KEY (data_id)
-        );
     ';
-    
+
     MAP::API->Exec($strSQLtable);
-    
+
     my $sel = undef;
-    
-    if (param('type') eq 'saved') {
-        $sel = MAP::API->SelectARef("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname."_tmp");
-    }
-    
-    if (param('type') eq 'submited') {
-        $sel = MAP::API->SelectARef("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname);
-    }
-    
+
+    $sel = MAP::API->SelectARef("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname);
+
     my $j = new JSON::Simple;
-    
+
     $j->Open('rows');
-    
+
     for(@$sel){
         $_->{user} = MAP::API->SelectOne("SELECT first_name FROM user_accounts WHERE user_id=?", $_->{user_id});
         $j->Object(
@@ -80,11 +65,11 @@ get '/'.$collectionName.'/:'.$primaryKey.'/:type.:format' => sub {
             }
         );
     }
-    
+
     $j->Close;
-    
+
     MAP::API->normal_header();
-    
+
     return $j->Print
 };
 
@@ -96,27 +81,20 @@ get '/'.$collectionName.'/:'.$primaryKey.'/:user_id/:type.:format' => sub {
     my $formname = MAP::API->SelectOne("SELECT formname FROM $tableName
                                    WHERE form_id = ?",
                                    $form_id) || MAP::API->fail( "formname not exist" );
-    
+
     my $sel = undef;
-    
-    if ($type eq 'saved') {
-        $sel = MAP::API->SelectRow("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname."_tmp
+
+    $sel = MAP::API->SelectRow("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname."
                                     WHERE user_id=?", $user_id);
-    }
-    
-    if ($type eq 'submited') {
-        $sel = MAP::API->SelectRow("SELECT * FROM dbo.formmaker_".$agency_id."_".$formname."
-                                    WHERE user_id=?", $user_id);
-    }
-    
+
     my $j = new JSON::Simple;
-    
+
     while (my ($key, $value) = each(%$sel)) {
         $j->Object($key => $value);
     }
-    
+
     MAP::API->normal_header();
-    
+
     return {
         status => 'success',
         response => 'Metadata for form '.$form_id.' was saved on ' . $collectionName,
@@ -132,19 +110,12 @@ del '/'.$collectionName.'/:'.$primaryKey.'/:type.:format' => sub {
     my $formname = MAP::API->SelectOne("SELECT formname FROM $tableName
                                    WHERE form_id = ?",
                                    $form_id) || MAP::API->fail( "formname not exist" );
-    
-    if ($type eq 'saved') {
-        MAP::API->Exec("DELETE FROM dbo.formmaker_".$agency_id."_".$formname."_tmp
+
+    MAP::API->Exec("DELETE FROM dbo.formmaker_".$agency_id."_".$formname."
                        WHERE user_id=?", $user_id);
-    }
-    
-    if ($type eq 'submited') {
-        MAP::API->Exec("DELETE FROM dbo.formmaker_".$agency_id."_".$formname."
-                       WHERE user_id=?", $user_id);
-    }
-    
+
     MAP::API->normal_header();
-    
+
     return {
         status => 'success',
         response => 'Deleted with sucess'
