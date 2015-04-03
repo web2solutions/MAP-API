3c4ca930ac2df2cf2ae65f6eeb5dbed783166643
web2solutions <eduardo@web2solutions.com.br>
2015-04-03 17:40:34 -0300

    new client session
    <br> - provide support for session on client side

--- lib/MAP/auth/Auth.pm
+++ lib/MAP/auth/Auth.pm
@@ -27,7 +27,7 @@ post '/auth.:format' => sub {
 	$auth = MIME::Base64::decode($auth) || &MAP::API->unauthorized("unable decode secret");
 
 
-    my ($salt_api_user, $salt_api_secret) = split(/:/, ($auth || ":"));
+  my ($salt_api_user, $salt_api_secret) = split(/:/, ($auth || ":"));
 	my $private_key  = request->header("User-Agent") || MAP::API->unauthorized("malformed headers");
 	my $user = MIME::Base64::decode( $salt_api_user ) || &MAP::API->unauthorized("unable decode salt_api_user");
 	#my @aUser = split(''.$private_key.'_', $userbase64);
@@ -42,12 +42,22 @@ post '/auth.:format' => sub {
 	my $username 	=  $user || &MAP::API->unauthorized("invalid username");
 	my $Origin  	= request->header("Origin") || MAP::API->unauthorized("malformed headers");
 	my $origin_status = "";
-	my $user_id = 0;
+
 	my $token = "";
 	my $first_name = "";
+	my $last_name = "";
+	my $title = "";
 	my $is_new_token = 0;
 	my $date_creation = 0;
 	my $date_expiration = 0;
+
+  my $user_id = 0;
+	my $user_type = undef;
+	my $connID = undef;
+	my $contact_id = undef;
+
+	my @applicants;
+	my $app2 = {};
 
 
 	my $dbh = MAP::API->dbh();
@@ -73,8 +83,10 @@ post '/auth.:format' => sub {
 	while ( my $record = $sth->fetchrow_hashref())
 	{
 		$secret_status = "ok";
-		$user_id = $record->{"user_id"};
-		$first_name = $record->{"first_name"};
+
+
+
+
 
 		my $user_salt_pass = sha256_hex( $private_key . '_' . $record->{"password"} );
 		if ( $user_salt_pass ne $salt_api_secret) {
@@ -83,6 +95,37 @@ post '/auth.:format' => sub {
 
 		if ( $record->{"status"} ne 'Active') {
 			MAP::API->unauthorized($record->{"status"} . " user")
+		}
+
+
+		$user_id = $record->{"user_id"};
+		$first_name = $record->{"first_name"};
+		$last_name = $record->{"last_name"};
+		$title = $record->{"title"};
+
+		$user_type = $record->{"user_type"};
+
+		$connID = $record->{"ConnId"};
+
+		debug $connID;
+
+		$contact_id = MAP::API->SelectOne(' select dbo.udf_CoupleContactId('.$connID.', 1)');
+
+		debug ' select dbo.udf_CoupleContactId('.$connID.', 1)';
+
+		debug $contact_id;
+
+		if ( $user_type eq 'adoptive_parent' ) {
+
+				my $connID_app2 = MAP::API->SelectOne('SELECT dbo.udf_CoupleConnId('.$connID.', 2)');
+				if ( defined($connID_app2) ) {
+						$app2->{'connid'} = $connID_app2;
+						$app2->{'contact_id'} = MAP::API->SelectOne(' select dbo.udf_CoupleContactId('.$connID_app2.', 2)');
+						$app2->{'user_type'} = 'adoptive_parent';
+						$app2->{'spouse_first_name'} = $record->{"spouse_first_name"};
+						$app2->{'spouse_last_name'} = $record->{"spouse_last_name"};
+
+				}
 		}
 	}
 
@@ -134,14 +177,34 @@ post '/auth.:format' => sub {
 	else
 	{
 		#my $reverse_username = reverse $username;
+
+
+		# types: adoptive_parent are clients
+		# types: agency_user,agency,admin are all caseowrker types
+		# if connid is on the table, then it is app1
+
+
+		# get connid of app 2 dbo.udf_CoupleConnId(ua1.ConnId, 2)
+
+
+
+		# [3/25/15, 3:22:48 PM] Vince Rossignol: select dbo.udf_CoupleContactId(ua1.ConnId, 1)
+		# [3/25/15, 3:22:57 PM] Vince Rossignol: dbo.udf_CoupleContactId(ua1.ConnId, 2)
+
 		my $auth_data = {
 			first_name =>	$first_name,
+			last_name =>	$last_name,
 			username => $username,
 			token => $token,
 			date_expiration => $date_expiration,
 			auth_status => $auth_status,
 			origin => $Origin,
-			client_session_id => $user_id
+			client_session_id => $user_id,
+			user_id => $user_id,
+			user_type => $user_type,
+			connID => $connID,
+			contact_id => $contact_id,
+			app2 => $app2
 		};
 
 		return {
